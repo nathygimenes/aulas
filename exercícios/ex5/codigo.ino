ea8d7080b209a523d4a5be6918d5631bf4b27c01
nathygimenes <78920605+nathygimenes@users.noreply.github.com>
2022-03-21 15:08:37 -0300

    Update codigo.ino

--- exercícios/ex5/codigo.ino
+++ exercícios/ex5/codigo.ino
@@ -1,1 +1,24 @@
 
+const int botao = 3;
+const int led = 2;
+int Estadobotao=0;
+
+
+void setup()
+ { 
+    pinMode(botao, INPUT);
+    pinMode(led, OUTPUT);
+   
+ }
+ 
+ void loop()
+ {
+   Estadobotao= digitalRead(botao);
+   if (Estadobotao ==HIGH){
+     digitalWrite (led, LOW);
+   }else{
+     digitalWrite(led,HIGH);
+   }
+ }
+   
+    
