04fc021a4a32a1e698552001bf1c83703aa8e576
nathygimenes <78920605+nathygimenes@users.noreply.github.com>
2022-03-21 15:05:25 -0300

    Update codigo.ino

--- exercícios/ex2/codigo.ino
+++ exercícios/ex2/codigo.ino
@@ -1,1 +1,19 @@
 
+void setup()
+{
+  pinMode(2, OUTPUT);
+  pinMode(4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(2, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(2, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+
+  digitalWrite(4, HIGH);
+  delay(1000); // Wait for 1000 millisecond(s)
+  digitalWrite(4, LOW);
+  delay(1000); // Wait for 1000 millisecond(s)
+}
